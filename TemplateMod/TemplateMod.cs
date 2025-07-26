ceff78b393bae6f42e3e550e9b9fc0f5802577d9
Miron Alexandru <mironalex@hotmail.com>
2025-07-27 01:19:29 +0300

    Swap back to ModInfo

--- TemplateMod/TemplateMod.cs
+++ TemplateMod/TemplateMod.cs
@@ -4,23 +4,23 @@ using UnityEngine;
 
 namespace TemplateMod;
 
-[BepInPlugin(PluginInfo.GUID, PluginInfo.NAME, PluginInfo.VERSION)]
+[BepInPlugin(ModInfo.GUID, ModInfo.NAME, ModInfo.VERSION)]
 [HarmonyPatch(typeof(TemplateMod), MethodType.Constructor)]
 public class TemplateMod : BaseUnityPlugin
 {
-    private readonly Harmony _harmony = new Harmony($"{PluginInfo.GUID}");
+    private readonly Harmony _harmony = new Harmony($"{ModInfo.GUID}");
 
     public TemplateMod()
     {
-        UnityEngine.Debug.Log($"{PluginInfo.NAME} constructed!");
+        UnityEngine.Debug.Log($"{ModInfo.NAME} constructed!");
     }
 
     private void Awake()
     {
-        UnityEngine.Debug.Log($"{PluginInfo.NAME} patching!");
+        UnityEngine.Debug.Log($"{ModInfo.NAME} patching!");
         _harmony.PatchAll();
 
-        UnityEngine.Debug.Log($"Hello from {PluginInfo.NAME}!");
+        UnityEngine.Debug.Log($"Hello from {ModInfo.NAME}!");
         UnityEngine.Debug.Log($"Application version is ${Application.version}.");
     }
 }
